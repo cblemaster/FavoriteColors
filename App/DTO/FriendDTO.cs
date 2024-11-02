e57bc7d7cc60adc0cc9dee0438abae76481a85e6
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-01 20:19:29 -0400

    added friend model and public dto in app project

---
+++ App/DTO/FriendDTO.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Drawing;
+
+namespace App.DTO;
+
+public class FriendDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+    public Color FavoriteColor { get; set; }
+}
---
+++ App/Models/Friend.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Drawing;
+
+namespace App.Models;
+
+internal class Friend
+{
+    internal uint Id { get; set; }
+    internal string FirstName {  get; set; } = string.Empty;
+    internal Color FavoriteColor { get; set; }
+}
