810031511e977409ba568f03dc1885b225ac395b
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-01 20:19:58 -0400

    added data service interface and implementation in app project

---
+++ App/Api/IDataService.cs
@@ -0,0 +1,14 @@
+﻿
+using App.DTO;
+
+namespace App.Api;
+
+public interface IDataService
+{
+    bool CheckForJsonDataFile();
+    void CreateJsonDataFile();
+    int GetMaxFriendId();
+    FriendDTO CreateFriend(FriendDTO newFriend);
+    FriendDTO[] GetFriendSearch(string searchString);
+    FriendDTO[] GetAllFriends();
+}
---
+++ App/Api/JsonDataService.cs
@@ -0,0 +1,15 @@
+﻿
+using App.DTO;
+
+namespace App.Api;
+
+public class JsonDataService : IDataService
+{
+    private readonly string _path = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\favorite-colors.json";
+    public bool CheckForJsonDataFile() => File.Exists(_path);
+    public void CreateJsonDataFile() => File.Create(_path);
+    public int GetMaxFriendId() => throw new NotImplementedException();
+    public FriendDTO CreateFriend(FriendDTO newFriend) => throw new NotImplementedException();
+    public FriendDTO[] GetFriendSearch(string searchString) => throw new NotImplementedException();
+    public FriendDTO[] GetAllFriends() => throw new NotImplementedException();
+}
