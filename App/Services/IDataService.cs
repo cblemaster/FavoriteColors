eab498143daab7bd03846f32c41fbee7fd947464
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-02 16:23:08 -0400

    last app project commit

---
+++ App/Services/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using App.DTO;
+
+namespace App.Services;
+
+public interface IDataService
+{
+    uint GetNewFriendId();
+    void CreateFriend(FriendDTO newFriend);
+    FriendDTO[] GetFriendSearch(string searchString);
+    FriendDTO[] GetAllFriends();
+}
---
+++ App/Services/JsonDataService.cs
@@ -0,0 +1,16 @@
+﻿using App.Models;
+using System.Collections.ObjectModel;
+
+namespace App.Services;
+
+public class JsonDataService
+{
+    private readonly FileService _fileService = new();
+    private IEnumerable<Friend> _allFriends;
+    
+    public ReadOnlyCollection<Friend> AllFriends => _allFriends.ToList().AsReadOnly();
+    private uint NextFriendId => AllFriends.Select(f => f.Id).Max() + 1;
+    
+    public void CreateFriend(Friend newFriend) => _allFriends.Append(newFriend);  // TODO>> may need to set id here
+    public Friend[] GetFriendSearch(string searchString) => AllFriends.Where(f => f.FirstName.Contains(searchString)).ToArray();
+}
