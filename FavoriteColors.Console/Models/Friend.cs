d02ee095b11d8f3ab7d848ad9bf72e18119d5ebd
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-02 16:25:48 -0400

    added friend model

---
+++ FavoriteColors.Console/Models/Friend.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace FavoriteColors.Console.Models;
+
+internal class Friend
+{
+    internal uint Id { get; set; }
+    internal string FirstName { get; set; } = string.Empty;
+    internal Color FavoriteColor { get; set; }
+}
