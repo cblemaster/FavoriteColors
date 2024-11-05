39cb6ddb43b3a986c7420b2a944618ca071308d2
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-04 20:17:07 -0500

    added data service interface and implementation to data project

---
+++ FavoriteColors.Data/IDataService.cs
@@ -0,0 +1,13 @@
+﻿
+using FavoriteColors.Data.Models;
+
+namespace FavoriteColors.Data;
+
+public interface IDataService
+{
+    IEnumerable<Friend> AllFriends { get; }
+    int GetIdForNewFriend();
+    void AddFriend(string firstName, string favColor);
+    IEnumerable<Friend> GetAllFriends();
+    IEnumerable<Friend> SearchFriends(string searchTerm);
+}
---
+++ FavoriteColors.Data/JsonDataService.cs
@@ -0,0 +1,23 @@
+﻿
+using FavoriteColors.Data.Models;
+
+namespace FavoriteColors.Data;
+
+public class JsonDataService : IDataService
+{
+    private readonly IEnumerable<Friend> _allFriends = [];
+    public IEnumerable<Friend> AllFriends => _allFriends.OrderBy(f => f.FirstName);
+
+    public int GetIdForNewFriend() => AllFriends.Any() ? AllFriends.Select(f => f.Id).Max() + 1 : 1;
+    public void AddFriend(string firstName, string favColor)
+    {
+        //TODO >> Validation
+        ConsoleColor c = Enum.GetValues<ConsoleColor>().SingleOrDefault(c => c.ToString().Equals(favColor, StringComparison.CurrentCultureIgnoreCase));
+        int nextFriendId = GetIdForNewFriend();
+        Friend newFriend = new() { Id = nextFriendId, FirstName = firstName, FavoriteColor = c.ToString() };
+
+        _ = _allFriends.Append(newFriend);
+    }
+    public IEnumerable<Friend> GetAllFriends() => AllFriends;
+    public IEnumerable<Friend> SearchFriends(string searchTerm) => AllFriends.Where(f => f.FirstName.Contains(searchTerm, StringComparison.CurrentCultureIgnoreCase));
+}
