ef3f690d3eacc0f8fc2428cf3264877b0ab7957a
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-04 20:16:38 -0500

    moved models from console project to data project

---
+++ FavoriteColors.Data/Models/Friend.cs
@@ -0,0 +1,9 @@
+﻿
+namespace FavoriteColors.Data.Models;
+
+public class Friend
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string FavoriteColor { get; set; } = string.Empty;
+}
