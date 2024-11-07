7c6a9fcfd34be2441aa55d4977bcf9dd7ba650dd
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-06 22:24:27 -0500

    added extension methods for UI

---
+++ FavoriteColors.Console/Extensions/UiExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace FavoriteColors.Console.Extensions;
+
+internal static class UiExtensions
+{
+    internal static void WriteToTerminal(this string s, int leadingNewLines = 0, int trailingNewLines = 0)
+    {
+        string start = new string('\n', leadingNewLines);
+        string middle = s;
+        string end = new string('\n', trailingNewLines);
+        string text = $"{start}{middle}{end}";
+        System.Console.WriteLine(text);
+    }
+    internal static bool IsValidMenuSelection(this char c) => Regex.IsMatch(c.ToString(), "[1 - 3]");
+    internal static bool IsValidName(this string s) => !string.IsNullOrWhiteSpace(s) && s.Length <= 15;
+    internal static bool IsValidFavColor(this string s) => !Enum.GetNames<ConsoleColor>().Select(c => c.ToLowerInvariant()).Contains(s.ToLowerInvariant());
+    internal static bool IsValidSearchTerm(this string s) => s.IsValidName();
+    internal static bool IsValidConfirmExit(this char c) => Regex.IsMatch(c.ToString(), "[y,Y,n,N]");
+    internal static bool HasLeadingOrTrailingWhiteSpace(this string s) => s.StartsWith(' ') || s.EndsWith(" ");
+    internal static string ToTrimmedString(this string s) => s.Trim();
+}
