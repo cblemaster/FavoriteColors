0d5893a4eccbeea0d4756d7715a146b88f27a616
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-15 17:13:28 -0500

    Revert "brought in files and code from console menus solution"
    This reverts commit 4362ad056b179183ca8e8afae6631f6eb3f2152e.

--- FavoriteColors.Console/MenuOption.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace FavoriteColors.Console;
-
-internal record MenuOption(string OptionLabel, int SortOrder, ConsoleKey[] SelectKeys, Action MenuAction)
-{
-    public override string ToString() => OptionLabel;
-}
