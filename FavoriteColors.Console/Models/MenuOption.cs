821468d3dc1a1b471c555598bf3a8be41bae00ba
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-15 17:45:33 -0500

    added files from console menus solution, some customizations

---
+++ FavoriteColors.Console/Models/MenuOption.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FavoriteColors.Console.Models;
+
+internal record MenuOption(string OptionLabel, int SortOrder, ConsoleKey[] SelectKeys, Action MenuAction)
+{
+    public Action MenuAction { get; private set; } = MenuAction;
+    public override string ToString() => OptionLabel;
+}
