3990ab9dfcbec020dd18c20787b5b1662aefd213
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-16 12:51:34 -0500

    added json service interface and implementation in api project

---
+++ FavoriteColors.API/Services/IJsonService.cs
@@ -0,0 +1,10 @@
+﻿
+using FavoriteColors.API.Models;
+
+namespace FavoriteColors.API.Services;
+
+internal interface IJsonService
+{
+    IEnumerable<Friend> DeserializeJsonToCollection(string json);
+    string SerializeCollectionToJson(IEnumerable<Friend> friends);
+}
---
+++ FavoriteColors.API/Services/JsonService.cs
@@ -0,0 +1,13 @@
+﻿using FavoriteColors.API.Models;
+using System.Text.Json;
+
+namespace FavoriteColors.API.Services
+{
+    internal class JsonService : IJsonService
+    {
+        public IEnumerable<Friend> DeserializeJsonToCollection(string json) =>
+            JsonSerializer.Deserialize<IEnumerable<Friend>>(json) ?? [];
+        public string SerializeCollectionToJson(IEnumerable<Friend> friends) =>
+            JsonSerializer.Serialize<IEnumerable<Friend>>(friends);
+    }
+}
