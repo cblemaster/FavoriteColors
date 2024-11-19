c5c3d1a41f9cf0183ac3453ebe82f2b35d5dd092
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-18 19:41:27 -0500

    changed props that I don't want to serialize into internal methods on friend model in api project

--- FavoriteColors.API/Models/Friend.cs
+++ FavoriteColors.API/Models/Friend.cs
@@ -7,7 +7,7 @@ public class Friend
     public string Name { get; set; } = string.Empty;
     public string FavoriteColor { get; set; } = string.Empty;
 
-    internal bool FriendIsValid => NameIsValid && FavColorIsValid;
-    internal bool NameIsValid => !string.IsNullOrWhiteSpace(Name) && Name.Length <= 15;
-    internal bool FavColorIsValid => Enum.GetNames<ConsoleColor>().Contains(FavoriteColor);
+    internal bool FriendIsValid() => NameIsValid() && FavColorIsValid();
+    internal bool NameIsValid() => !string.IsNullOrWhiteSpace(Name) && Name.Length <= 15;
+    internal bool FavColorIsValid() => Enum.GetNames<ConsoleColor>().Contains(FavoriteColor);
 }
