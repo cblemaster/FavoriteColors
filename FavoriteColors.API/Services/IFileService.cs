1ae040a7a1e23380ebaa00a16137fe6bc83235e4
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-20 18:10:38 -0500

    renamed project folder and removed api project

--- FavoriteColors.API/Services/IFileService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FavoriteColors.API.Services;
-
-public interface IFileService
-{
-    bool TryCreateDirectoryIfNotExists();
-    bool TryCreateFileIfNotExists();
-    string TryReadFile();
-    bool TryWriteFile(string text);
-}
