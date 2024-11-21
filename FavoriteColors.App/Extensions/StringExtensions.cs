e330d6a90a265c881c4ab453b903dc15c6a3c52e
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-20 21:17:07 -0500

    added string extensions

---
+++ FavoriteColors.App/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace FavoriteColors.App.Extensions;
+
+public static class StringExtensions
+{
+    public static void WriteToTerminal(this string s, int leadingNewLines = 1, int trailingNewLines = 1)
+    {
+        string leading = leadingNewLines > 0 ? new('\n', leadingNewLines) : string.Empty;
+        string trailing = trailingNewLines > 0 ? new('\n', trailingNewLines) : string.Empty;
+        string result = $"{leading}{s}{trailing}";
+        Console.WriteLine(result);
+    }
+}
