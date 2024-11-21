46c0a16ee5ffe18cee067cdec0a256e9777788c6
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-21 18:22:53 -0500

    added extension method to write sequences of friends to the terminal as a table

---
+++ FavoriteColors.App/Extensions/IReadOnlyCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿
+using FavoriteColors.App.Models;
+
+namespace FavoriteColors.App.Extensions;
+
+public static class IReadOnlyCollectionExtensions
+{
+    public static void WriteToTerminal(this IReadOnlyCollection<Friend> friends)
+    {
+        string heading = "Friend name \tFavoriteColor";
+
+        heading.WriteToTerminal();
+        friends.ToList().ForEach(friend => friend.ToString().WriteToTerminal());
+    }
+}
