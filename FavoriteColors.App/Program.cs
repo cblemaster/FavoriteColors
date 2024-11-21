09a14c2f11ba769b2df50e8f69abf41056c3b60e
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-20 21:03:35 -0500

    pseudo code in program.cs

--- FavoriteColors.App/Program.cs
+++ FavoriteColors.App/Program.cs
@@ -1,3 +1,18 @@
 ﻿
 Console.WriteLine();
 
+// set fields for friends data and file handler
+// filehandler.readfile
+// pass string to data.fromjson to set allfriends
+// start ui
+// show intro
+// get menu and options
+// assign option actions
+// run menu
+// show options
+// prompt for option
+// perform option action
+// run menu
+// pass allfriends to data.tojson
+// pass this string to file.writefile
+// show outro
