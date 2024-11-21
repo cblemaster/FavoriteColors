9acc501e446e6cc7e4e02f2463d38a1484bafb58
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-20 20:35:39 -0500

    added menu definition and menu option definition classes

---
+++ FavoriteColors.App/Models/MenuDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColors.App.Models;
+
+public record MenuDefinition
+(
+    string Title,
+    IEnumerable<MenuOptionDefinition> MenuOptionDefinitions,
+    IEnumerable<MenuDefinition> SubMenuDefinitions,
+    string Prompt,
+    string InputValidationError,
+    ConsoleKey ExitKey,
+    string ExitKeyString
+);
---
+++ FavoriteColors.App/Models/MenuOptionDefinition.cs
@@ -0,0 +1,14 @@
+﻿namespace FavoriteColors.App.Models;
+
+public record MenuOptionDefinition
+(
+    int MenuOptionId,
+    string OptionLabel,
+    int SortOrder,
+    ConsoleKey[] SelectKeys,
+    Action MenuAction
+)
+{
+    public Action MenuAction { get; private set; } = MenuAction;
+    public void SetMenuAction(Action action) => MenuAction = action;
+}
