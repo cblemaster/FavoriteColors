58fbe0a4653fe7c47ed51f5b4979c9d7fa22e3c1
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-23 10:12:36 -0500

    updated extension method to work on a higher level abstraction

--- FavoriteColors.App/Extensions/IEnumerableExtensions.cs
+++ FavoriteColors.App/Extensions/IEnumerableExtensions.cs
@@ -3,10 +3,16 @@ using FavoriteColors.App.Models;
 
 namespace FavoriteColors.App.Extensions;
 
-public static class IReadOnlyCollectionExtensions
+public static class IEnumerableExtensions
 {
-    public static void WriteToTerminal(this IReadOnlyCollection<Friend> friends)
+    public static void WriteToTerminal(this IEnumerable<Friend> friends)
     {
+        if (!friends.Any())
+        {
+            string none = "No friends found!";
+            none.WriteToTerminal();
+        }
+        
         string heading = "Friend name \tFavoriteColor";
 
         heading.WriteToTerminal();
